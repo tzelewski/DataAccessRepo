dfdb13ec12a4ddd6d0b82c7f7e6b2e6e3d7c2312
tomasz zelewski <tomasz.zelewski@blueprism.com>
2020-04-15 13:32:11 +0100

    initial commit

---
+++ DataAccessRepo/DataAccessRepository.cs
@@ -0,0 +1,53 @@
+﻿namespace DataAccessRepo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.Common;
+    using System.Threading.Tasks;
+
+    public class DataAccessRepository : IDbRepository
+    {
+        public Func<IDbConnection> GetDbConnection { get; }
+
+        public DataAccessRepository(Func<IDbConnection> getDbConnection)
+        {
+            GetDbConnection = getDbConnection;
+        }
+
+        public async Task<IList<T>> ExecuteQuery<T>(Func<DbDataReader, T> getResult, string query,
+            params (string parameterName, string value)[] parameters)
+        {
+            var resultList = new List<T>();
+            var connection = GetDbConnection();
+
+            await using var command = (DbCommand)connection.CreateCommand();
+
+            command.CommandText = query;
+
+            foreach (var (parameterName, value) in parameters)
+            {
+                var parameter = command.CreateParameter();
+                parameter.ParameterName = parameterName;
+                parameter.Value = value;
+                command.Parameters.Add(parameter);
+            }
+
+            connection.Open();
+
+            await using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                resultList.Add(getResult(reader));
+            }
+
+            return resultList;
+        }
+    }
+
+    public interface IDbRepository
+    {
+        Task<IList<T>> ExecuteQuery<T>(Func<DbDataReader, T> getResult, string query,
+            params (string parameterName, string value)[] parameters)
+    }
+}
---
+++ DataAccessRepo/IDbRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAccessRepo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Common;
+    using System.Threading.Tasks;
+
+    public interface IDbRepository
+    {
+        Task<IList<T>> ExecuteQuery<T>(Func<DbDataReader, T> getResult, string query,
+            params (string parameterName, string value)[] parameters);
+    }
+}
